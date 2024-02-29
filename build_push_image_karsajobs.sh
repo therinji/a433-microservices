bf38eb9681299777bdccbdbe95411c731b27b0e4
therinji <rindho2704@gmail.com>
2024-02-29 07:40:47 +0700

    script untuk build dan push docker image

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# build docker image
+docker build -t ghcr.io/therinji/karsajobs:latest .
+# export PAT=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
+echo $PAT | docker login ghcr.io --username therinji --password-stdin
+# mengunggah image ke GitHub Packages
+docker push ghcr.io/therinji/karsajobs:latest
