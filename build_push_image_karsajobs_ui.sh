36629ea97fbcc3473ababe01a1a6078d7957647f
therinji <rindho2704@gmail.com>
2024-03-07 23:17:04 +0700

    update build push script

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # build docker image
 docker build -t ghcr.io/therinji/karsajobs-ui:latest .
-# export PAT=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
+# export PAT=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
 echo $PAT | docker login ghcr.io --username therinji --password-stdin
 # mengunggah image ke GitHub Packages
 docker push ghcr.io/therinji/karsajobs-ui:latest
