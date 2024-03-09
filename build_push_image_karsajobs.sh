451027eed0976d1a31a0cd52a768e092dc8029b0
therinji <rindho2704@gmail.com>
2024-03-09 12:34:26 +0700

    last update configuration

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # build docker image
 docker build -t ghcr.io/therinji/karsajobs:latest .
-# export PAT=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
+# login ke Github Packages
 echo $PAT | docker login ghcr.io --username therinji --password-stdin
 # mengunggah image ke GitHub Packages
 docker push ghcr.io/therinji/karsajobs:latest
